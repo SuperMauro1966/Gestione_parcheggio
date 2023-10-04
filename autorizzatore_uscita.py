779748b67da1496aaa8ca8e53c40d6d208b9d8dd
riki34r <riccardo.nescito@gmail.com>
2023-10-04 11:56:48 +0200

    introdotto query recupero dati ticket, impostata regola di uscita entro 5 minuti - work in progress

--- autorizzatore_uscita.py
+++ autorizzatore_uscita.py
@@ -1,4 +1,18 @@
-import random
-def e_autorizzato_a_uscire(id_ticket):
-   return random.choice((True,False))
+import datetime
 
+def e_autorizzato_a_uscire(id_ticket, conn):
+   cursor= conn.cursor()
+   cursor.execute("""
+             SELECT OrarioEntrata, NOW() AS adesso 
+            FROM ticket 
+            WHERE IDTicket = ?""", (id_ticket,)) 
+   if cursor.rowcount != 1:
+      return False 
+   
+   orario_ingresso, adesso  = cursor.fetchone() 
+  
+
+   differenza_tempo = adesso - orario_ingresso
+   if differenza_tempo <= datetime.timedelta(minutes=5):
+         return True
+  #verifica altre regole per l'uscita 
