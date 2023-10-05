cd0232a2b804abebe054ad44aabf5d9a91050bd8
samufrasso <samuelefrasso@gmail.com>
2023-10-05 12:31:34 +0200

    completato la scrittura per le condizioni di uscita

--- autorizzatore_uscita.py
+++ autorizzatore_uscita.py
@@ -3,16 +3,25 @@ import datetime
 def e_autorizzato_a_uscire(id_ticket, conn):
    cursor= conn.cursor()
    cursor.execute("""
-             SELECT OrarioEntrata, NOW() AS adesso 
+            SELECT OrarioEntrata, NOW() AS adesso,importo,OrarioUscita 
             FROM ticket 
             WHERE IDTicket = ?""", (id_ticket,)) 
    if cursor.rowcount != 1:
       return False 
    
-   orario_ingresso, adesso  = cursor.fetchone() 
-  
-
+   orario_ingresso, adesso, importo,orario_uscita = cursor.fetchone() 
    differenza_tempo = adesso - orario_ingresso
    if differenza_tempo <= datetime.timedelta(minutes=5):
          return True
+   
   #verifica altre regole per l'uscita 
+   if importo < 0:   #importo non pagato
+       return False
+   if orario_uscita is None:
+       return False
+   differenza_uscita = orario_uscita-adesso
+   
+   if differenza_uscita<=datetime.timedelta(minutes=5):
+         return True
+   
+   return False
