d1d6c879d454777b385d8a3a0651a3abfbd0f656
samufrasso <samuelefrasso@gmail.com>
2023-10-11 12:29:56 +0200

    introdotto codice di stato

--- autorizzatore_uscita.py
+++ autorizzatore_uscita.py
@@ -1,6 +1,12 @@
 import datetime
+from enum import Enum
 
-
+class AutUscitaStatus(Enum):
+   AUTORIZZATO = 1
+   TICKET_NON_TROVATO = 2
+   IMPORTO_NON_PAGATO = 3
+   NO_ORARIO_USCITA = 4
+   TEMPO_MASSIMO_SCADUTO = 5
 
 def e_autorizzato_a_uscire(id_ticket, conn):
    cursor= conn.cursor()
@@ -11,23 +17,23 @@ def e_autorizzato_a_uscire(id_ticket, conn):
                ON t.IDVeicolo=v.IDveicolo
                WHERE IDTicket = ? ;""", (id_ticket,)) 
    if cursor.rowcount != 1:
-      return False 
+      return AutUscitaStatus.TICKET_NON_TROVATO 
    
    orario_ingresso, adesso, importo,orario_uscita,id_tipologia= cursor.fetchone() 
    differenza_tempo = adesso - orario_ingresso
    if differenza_tempo <= datetime.timedelta(minutes=5):
-         return True
+         return AutUscitaStatus.AUTORIZZATO
    
    #verifica altre regole per l'uscita 
-   if importo < 0:   #importo non pagato
-      return False
+   if importo <= 0:   #importo non pagato
+      return AutUscitaStatus.IMPORTO_NON_PAGATO
    if orario_uscita is None:
-      return False
+      return AutUscitaStatus.NO_ORARIO_USCITA
    differenza_uscita = orario_uscita-adesso
    
    if differenza_uscita>datetime.timedelta(minutes=5):
-         return False
-   return True
+         return AutUscitaStatus.TEMPO_MASSIMO_SCADUTO
+   return AutUscitaStatus.AUTORIZZATO
 
 
 
