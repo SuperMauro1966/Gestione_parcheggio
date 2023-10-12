d5f30940c29caaed7a6a3867a6f1ed7ec21f21cd
samufrasso <samuelefrasso@gmail.com>
2023-10-12 12:28:12 +0200

    inserita gestione messaggi in più lingue

--- totem_uscita.py
+++ totem_uscita.py
@@ -17,20 +17,15 @@ class TotemUscita:
 
     def __init__(self):
         self.conn=self.get_db_connection()
-        self.messaggi={
-            au.AutUscitaStatus.AUTORIZZATO:"ARRIVEDERCI E GRAZIE ",
-            au.AutUscitaStatus.IMPORTO_NON_PAGATO:"NON AUTORIZZATO AD USCIRE PERCHE L'IMPORTO NON E STATO PAGATO",
-            au.AutUscitaStatus.NO_ORARIO_USCITA:"",
-            au.AutUscitaStatus.TEMPO_MASSIMO_SCADUTO:"",
-            
-        }
+        self.messaggi=self.get_messaggi()
 
+       
     def get_db_connection(self):
         try:
             return  mariadb.connect(
                 user="root",
                 host="127.0.0.1",
-                port=3306,
+                port=3308,
                 password="12345678",
                 database="db_parcheggio"
 
@@ -38,14 +33,39 @@ class TotemUscita:
         except mariadb.Error as e:
             print(f": {e}")
             sys.exit(1)
+
+    def get_messaggi(self):
+
+        cursor= self.conn.cursor()
+        cursor.execute(""" 
+                SELECT Contenuto,Lingua
+                FROM messaggi AS m
+                       """)
+        db_messaggi = {}
+        for riga in cursor:
+            contenuto, lingua = riga 
+            if ":" not in contenuto:
+                continue
+            try:
+                dizionario = db_messaggi[lingua]
+            except KeyError:
+                db_messaggi[lingua] = dizionario = {}
+                
+            
+            prefisso, suffisso = contenuto.split(':',maxsplit=1)
+            for status in au.AutUscitaStatus:
+                if status.name == prefisso:
+                    dizionario[status]=suffisso
+                    break
         
+        return db_messaggi
+
 
     def start(self):
-        
         while True:
             ds.mostra_messaggio(self.WELCOME)
             id_ticket= lt.get_id_ticket()
-            status=au.e_autorizzato_a_uscire(id_ticket)
+            status=au.e_autorizzato_a_uscire(id_ticket, self.conn)
             if status == au.AutUscitaStatus.AUTORIZZATO :
                 ds.mostra_messaggio(self.messaggi.get(status,"???"))
                 sb.permetti_uscita()
