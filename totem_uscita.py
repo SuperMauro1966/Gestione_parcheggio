a7719cab1bf7e8bc6a3d17775219c6476033777d
samufrasso <samuelefrasso@gmail.com>
2023-10-13 11:19:57 +0200

    introdotto gestione messaggi in diverse lingue

--- totem_uscita.py
+++ totem_uscita.py
@@ -10,10 +10,6 @@ import sbarra as sb
 import display as ds
 
 class TotemUscita:
-    WELCOME="Benvuto, inserisci il tuo ticket "
-    BYEBYE= "Arrivederci e grazie! "
-    NOT_ALLOWED="Non autorizzato all'uscita "
-
 
     def __init__(self):
         self.conn=self.get_db_connection()
@@ -34,48 +30,66 @@ class TotemUscita:
             print(f": {e}")
             sys.exit(1)
 
+    
     def get_messaggi(self):
-
         cursor= self.conn.cursor()
         cursor.execute(""" 
                 SELECT Contenuto,Lingua
                 FROM messaggi AS m
-                       """)
+                    """)
+
+        dati_messaggi=(( contenuto.split(":", maxsplit=1)[0],
+                        contenuto.split(":", maxsplit=1)[1],
+                          lingua)
+                        for contenuto, lingua in cursor if ":" in contenuto)
+
         db_messaggi = {}
-        for riga in cursor:
-            contenuto, lingua = riga 
-            if ":" not in contenuto:
-                continue
+        authstatus_by_name = { status.name : status for status in au.AutUscitaStatus}
+
+        for status_name, message, lingua in dati_messaggi:
             try:
                 dizionario = db_messaggi[lingua]
             except KeyError:
                 db_messaggi[lingua] = dizionario = {}
                 
-            
-            prefisso, suffisso = contenuto.split(':',maxsplit=1)
-            for status in au.AutUscitaStatus:
-                if status.name == prefisso:
-                    dizionario[status]=suffisso
-                    break
-        
+            dizionario[authstatus_by_name.get(status_name,"???")]= message
+
         return db_messaggi
+
+
 
 
     def start(self):
         while True:
-            ds.mostra_messaggio(self.WELCOME)
-            id_ticket= lt.get_id_ticket()
-            status=au.e_autorizzato_a_uscire(id_ticket, self.conn)
+            lingua = self._selezione_lingue()
+            id_ticket = lt.get_id_ticket()
+            status = au.e_autorizzato_a_uscire(id_ticket, self.conn)
             if status == au.AutUscitaStatus.AUTORIZZATO :
-                ds.mostra_messaggio(self.messaggi.get(status,"???"))
+                ds.mostra_messaggio(self.messaggi[lingua].get(status,"???"))
                 sb.permetti_uscita()
             else:
-                ds.mostra_messaggio(self.messaggi.get(status,"???"))
+                ds.mostra_messaggio(self.messaggi[lingua].get(status,"???"))
 
             time.sleep(10)
 
-totem_uscita=TotemUscita()
-totem_uscita.start()
 
+    def _selezione_lingua(self):
+        ds.mostra_messaggio("lingue disponibili")
+        pos_lang_sequence = enumerate(self.messaggi.keys(),start= 1) 
+        scelta_lingue = dict(pos_lang_sequence)
+        for num, codice_lingua in pos_lang_sequence :
+            ds.mostra_messaggio(str(num) + "-" + codice_lingua)
+        ds.mostra_messaggio("Seleziona Lingua")
 
+        lingua = None
+        while lingua is None:
+            lingua = scelta_lingue.get(ds.pulsante_premuto(),None) 
+
+        return lingua
         
+
+if __name__ == "__main__":
+    totem_uscita=TotemUscita()
+    totem_uscita.start()
+
+
